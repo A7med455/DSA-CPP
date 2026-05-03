b0b4b6d9015f4755f23c7d21a15bbce8fab4d528
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-05-03 20:39:07 +0300

    New Methods Added

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Stack
 {
 private:
+
     int* arr;
     int top;
     int capacity;
@@ -11,14 +12,24 @@ private:
 public:
     Stack(int size) // Initialized an empty Stack
     {
-        capacity=size;
-        top= -1; // start with -1 bec no elements yet
-        arr=new int[capacity];
+        capacity = size;
+        top = -1; // start with -1 bec no elements yet
+        arr = new int[capacity];
     }
     ~Stack()
     {
         delete[] arr;
         cout<<"Stack is Destroyed "<<endl;
+    }
+
+     bool isEmpty()
+    {
+        return top == -1;
+    }
+
+    int size()
+    {
+        return top + 1;
     }
     void push(int newItem)
     {
@@ -29,6 +40,7 @@ public:
         }
         top++;
         arr[top]=newItem;
+        counter++;
         cout<<"Pushed:"<<newItem<<endl;
     }
 
@@ -41,6 +53,7 @@ public:
         }
         int poppedValue = arr[top];
         top--; 
+        counter--;
         return poppedValue;
     }
     int peek()
@@ -55,10 +68,51 @@ public:
 
 };
 
+Stack reverse(Stack s1,int size)
+{
+    Stack s2(size);
+    while(!s1.isEmpty())
+    {  
+        s2.push(s1.pop());
+    }
+    return s2;
+}
+  
+
+Stack merge(Stack s1,Stack s2,int size1,int size2)
+{
+    Stack s3(size1 + size2);
+
+    while(!s1.isEmpty())
+    {
+        s3.push(s1.pop());
+    }
+    while(!s2.isEmpty())
+    {
+        s3.push(s2.pop());
+    }
+    
+    Stack s4(size1 + size2);
+
+    while(!s3.isEmpty())
+    {
+        s4.push(s3.pop());
+    }
+    return s4;
+}
+
 int main()
 {
     
+Stack s1(5);
+s1.push(10);
+s1.push(20);
+s1.push(30);
+s1.push(40);
+s1.push(50);
+Stack s2(5);
 
+s2=reverse(s1,5);
 
 
     return 0;
